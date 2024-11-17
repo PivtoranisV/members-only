52d2c65d0ba09c4d913ff9660f2d213b7454a002
Volodymyr Pivtoranis <pivtoranis87@gmail.com>
2024-11-17 17:36:13 +0200

    Add create post method

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,25 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!, only: [ :new, :create ]
+
+  def index
+  end
+
   def new
+    @post = Post.new
   end
 
   def create
+    @post = current_user.posts.build(post_params)
+    if @post.save
+      redirect_to posts_path, notice: "Post was successfully created."
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
-  def index
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body)
   end
 end
